25fc56ff0c8592c0400b0fc29e35e5f67050145f
Jacob Pratt <jpratt@holtic.com>
2026-06-05 09:15:24 -0700

    finished basic project

---
+++ data/README.md
@@ -0,0 +1,22 @@
+This is where Form 41 Schedule T2 data files will be placed.
+
+To update the dataset:
+
+1.  Go to https://transtats.bts.gov/
+2.  Click on "Aviation" on the sidebar
+3.  Click on "Air Carrier Summary Data (Form 41 and 298C Summary Data)"
+4.  Click on "T2: U.S. Air Carrier TRAFFIC And Capacity Statistics by Aircraft Type"
+5.  Click on "Download" on the sidebar
+6.  Select, at minimum, the following fields: `Year`, `UniqueCarrier`, `AirHours`, `AirDaysEquip`
+7.  Near the top, specify "All Years" in the "Filter Year" field
+8.  Click on the "Download" button
+9.  Unzip the downloaded file and replace `T_SCHEDULE_T2.csv` in this directory with its contents
+10. Run the program in the parent directory to obtain updated calculations
+
+
+Required fields for download:
+
+-   `Year`
+-   `UniqueCarrier`
+-   `AirHours`
+-   `AirDaysEquip`
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include "rapidcsv.hpp"
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+
+// Calculates the number of days in a given year
+short daysInYear(short year) {
+    // If this is a leap year, return 366. Otherwise return 365.
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 366 : 365;
+}
+
+int main() {
+    std::cout << "Reading data... " << std::flush;
+    rapidcsv::Document t2("data/T_SCHEDULE_T2.csv");   // open the T2 data file
+    
+    // Load data columns from the table
+    std::vector<short> years = t2.GetColumn<short>("YEAR");
+    std::vector<std::string> hours = t2.GetColumn<std::string>("HOURS_AIRBORNE_650");
+    std::vector<std::string> days  = t2.GetColumn<std::string>("AIR_DAYS_EQUIP_810");
+    std::cout << "done." << std::endl; // done reading data
+
+
+    // Data structure to store data in. Maps year to <days, hours>
+    typedef std::map<short, std::pair<double, double>> flightRecorder;
+    flightRecorder data; // data is a flightRecorder (map)
+
+
+    // Iterate through table rows and collect relevant information
+    for (size_t i = 0; i < years.size(); ++i) {
+        short year = years[i];
+        if (days[i]  != "") data[year].first  += std::stod(days[i]);
+        if (hours[i] != "") data[year].second += std::stod(hours[i]);
+    }
+
+
+    // Iterate through map keys (data years), calculate airborne hours and return
+    std::cout << "\n\nAVERAGE ANNUAL AIRCRAFT AIRBORNE HOURS PER YEAR:\n\n";
+    std::cout << "YEAR\tAVERAGE AIRCRAFT AIRBORNE HOURS\n";
+    for (flightRecorder::iterator it = data.begin(); it != data.end(); ++it) {
+        short  year = it->first;
+        double acDays = it->second.first;                        // unit: aircraft * day
+        double acFlHr = it->second.second;                       // unit: aircraft * flight hour
+        double annualHours = daysInYear(year) * acFlHr / acDays; // unit: flight hour / year
+        std::cout << year << '\t' << annualHours << '\n';
+    }
+}
